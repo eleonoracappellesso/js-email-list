429da5b6900b47378a002eb5ada0269d4b2af8e6
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-18 16:17:01 +0100

    created a cicle for to generale 10 mails at click on button

--- script/script.js
+++ script/script.js
@@ -6,3 +6,34 @@ Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
+
+const generateMailBtn = document.getElementById('mailBtn');
+const emailsUl = document.getElementById('mailsList');
+
+generateMailBtn.addEventListener("click", generateMails);
+
+
+
+function generateMails() {
+    const emailsList = [];
+
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((mails) => {
+                console.log(mails.data);
+                emailsList.push(mails.data.response);
+
+                if (emailsList === 10) {
+                    let emailLi = "";
+                    emailsList.forEach(element => {
+                        emailLi += `<li>${element}</li>`;
+                    });
+
+                    emailsUl.innerHTML = emailLi;
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+}
